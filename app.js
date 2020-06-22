05dde21a1e792f339d8df9f9a86c7ccffa64faad
777Vikki <52693335+777Vikki@users.noreply.github.com>
2020-06-22 00:11:45 -0400

    Add files via upload
    CRUD query with mongodb using mongoose

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(
+  "mongodb+srv://nodejs:" +
+    process.env.MONGO_ATLAS_PW +
+    "@cluster0-fyxor.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+// Database code are written inside products.js file
+// Referrence: https://www.youtube.com/watch?v=WDrU305J1yw&list=PL55RiY5tL51q4D-B63KBnygU6opNPFk_q&index=6
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
